dc74b9f117dae8567b2acd149a26a0b008a2937c
Sourya Mishra <sourya.mishra111@gmail.com>
2024-01-15 19:07:20 +0530

    setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Hotel Management App",
+  description: "Discover the best hotel rooms",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <main className="font-normal">{children}</main>
+      </body>
+    </html>
+  );
+}
